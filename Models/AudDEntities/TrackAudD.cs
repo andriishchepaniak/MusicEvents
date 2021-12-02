385f0a4707c2e74f6c29c8e0f01eadaa18655130
andrii shchepaniak <andrew.shtchepanyak@gmail.com>
2021-12-02 15:25:50 +0200

    Add Track from AudD

---
+++ Models/AudDEntities/TrackAudD.cs
@@ -0,0 +1,21 @@
+﻿using Models.SpotifyEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.AudDEntities
+{
+    public class TrackAudD
+    {
+        public string artist { get; set; }
+        public string title { get; set; }
+        public string album { get; set; }
+        public string release_date { get; set; }
+        public string label { get; set; }
+        public string timecode { get; set; }
+        public string song_link { get; set; }
+        public TrackSpotify spotify { get; set; }
+    }
+}
